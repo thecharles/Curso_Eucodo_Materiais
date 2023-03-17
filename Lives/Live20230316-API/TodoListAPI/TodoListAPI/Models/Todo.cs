b79b1106dccd582257aa95ff6de1264821017a61
Charles Silva <charles.augusto.silva@gmail.com>
2023-03-16 21:43:08 -0300

    Nova live

---
+++ Lives/Live20230316-API/TodoListAPI/TodoListAPI/Models/Todo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoListAPI.Models;
+
+public partial class Todo
+{
+    public int Id { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public DateTime? DataInclusao { get; set; }
+}
---
+++ Lives/Live20230316-API/TodoListAPI/TodoListAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoListAPI.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string Senha { get; set; } = null!;
+
+    public DateTime DataInclusao { get; set; }
+}
