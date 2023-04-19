b1db7bc7c7a2055a860d2167a08334cab1d9116a
Charles Silva <charles.augusto.silva@gmail.com>
2023-04-19 16:43:31 -0300

    Incluido o codigo da entrevista demonstrado na live

---
+++ Lives/Live20230418-full-live/avancado/CandidatoTestes/ConsoleApp1/CDNLog.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateTesting.CharlesAugustoSilva.Converters
+{
+    /// <summary>
+    /// Responsiable to convert the string into an object.
+    /// </summary>
+    public class CDNLog
+    {
+        public string ResponseSize { get; set; }
+        public string StatusCode { get; set; }
+        public string CacheStatus { get; set; }
+        public string RequestLine { get; set; }
+        public decimal TimeTaken { get; set; }
+        public string HttpMethod { get; set; }
+        public string UriPath { get; set; }
+
+        public bool IsValid { get { return StatusCode != null; } }
+
+        public CDNLog() { }
+
+        public CDNLog(string logText)
+        {
+            string[] parts = logText.Split('|');
+            if (parts.Length != 5)
+                return;
+
+            this.ResponseSize = parts[0];
+            this.StatusCode = parts[1];
+            this.CacheStatus = parts[2];
+            this.RequestLine = parts[3].Trim('"');
+            this.TimeTaken = decimal.Parse(parts[4].Replace(".", ","));
+
+            string[] requestParts = this.RequestLine.Split(' ');
+            this.HttpMethod = requestParts[0];
+            this.UriPath = requestParts[1];
+
+            if (this.CacheStatus == "INVALIDATE")
+            {
+                this.CacheStatus = "REFRESH_HIT";
+            }
+        }
+    }
+}
---
+++ Lives/Live20230418-full-live/avancado/CandidatoTestes/MyTests/ConvertCDNLogToAgoraFormatTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateTesting.CharlesAugustoSilva.Converters.Tests
+{
+    public  class ConvertCDNLogToAgoraFormatTests
+    {
+        [Fact]
+        public void TestLineHit()
+        {
+            var obj = new CDNLog("312|200|HIT|\"GET /robots.txt HTTP/1.1\"|100.2");
+            string agoraFormat = new ConvertCDNLogToAgoraFormat().Convert(obj);
+            Assert.True(agoraFormat == "\"MINHA CDN\" GET 200 /robots.txt 100 312 HIT");
+        }
+    }
+}
